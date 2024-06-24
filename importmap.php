2f4cc6ac333e4b100bd75916c630dbc6ac99a413
Romain LETHUMIER <romain.lethumier@gmail.com>
2024-06-24 17:16:11 +0200

    Add Symfony Mercure and Webpack Encore bundles

--- importmap.php
+++ importmap.php
@@ -19,10 +19,8 @@ return [
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-    ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
+    'tom-select/dist/css/tom-select.default.css' => [
+        'version' => '2.3.1',
+        'type' => 'css',
     ],
 ];
