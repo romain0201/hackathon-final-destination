090e41ccb7698288268d9f513d9078cd226f90d4
Romain LETHUMIER <romain.lethumier@gmail.com>
2024-06-25 00:02:11 +0200

    Updated channel chat functionality and login credentials

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -5,19 +5,29 @@ namespace App\Entity;
 use App\Repository\MessageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\UX\Turbo\Attribute\Broadcast;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
-#[Broadcast]
 class Message
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['message'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['message'])]
     private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    #[Groups(['message'])]
+    private ?User $author = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    #[Groups(['message'])]
+    private ?Channel $channel = null;
 
     public function getId(): ?int
     {
@@ -32,6 +42,30 @@ class Message
     public function setContent(string $content): static
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getChannel(): ?Channel
+    {
+        return $this->channel;
+    }
+
+    public function setChannel(?Channel $channel): static
+    {
+        $this->channel = $channel;
 
         return $this;
     }
