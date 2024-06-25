ffcb88299848376dcb0e86d6dbaad9c83ff3debc
Romain LETHUMIER <romain.lethumier@gmail.com>
2024-06-25 12:01:03 +0200

    Add message rendering and sending functionality to chat

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -19,12 +20,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['message'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['message'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['message'])]
     private ?string $name = null;
 
     public function getName(): ?string
