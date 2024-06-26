0f4676210993bd742a7005a97aaf250dc0b78504
Romain LETHUMIER <romain.lethumier@gmail.com>
2024-06-26 16:19:40 +0200

    Update JDBC URL and refactor message handling

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Message;
 use App\Repository\ChannelRepository;
+use App\Repository\UserRepository;
+use App\Service\OllamaClient;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +20,11 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class MessageController extends AbstractController
 {
+    public function __construct()
+    {
+        $this->ollamaClient = new OllamaClient();
+    }
+
     #[Route('/message', name: 'message', methods: ['POST'])]
     public function sendMessage(
         Request $request,
@@ -42,6 +49,64 @@ class MessageController extends AbstractController
         $message->setContent($content);
         $message->setChannel($channel);
         $message->setAuthor($this->getUser());
+        $date = new DateTime();
+        $message->setCreatedAt($date);
+        $message->setUpdatedAt($date);
+
+        $em->persist($message);
+        $em->flush();
+
+        $jsonMessage = $serializer->serialize($message, 'json', [
+            'groups' => ['message']
+        ]);
+
+        $update = new Update(
+            sprintf('https://localhost/conversation/%s',
+                $channel->getId()),
+            $jsonMessage,
+            true
+        );
+
+        $hub->publish($update);
+
+        return new JsonResponse(
+            $jsonMessage,
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    #[Route('/message-ia', name: 'message-ia', methods: ['POST'])]
+    public function askMistral(Request $request,
+                               ChannelRepository $channelRepository,
+                               UserRepository $userRepository,
+                               SerializerInterface $serializer,
+                               EntityManagerInterface $em,
+                               HubInterface $hub)
+    {
+        $data = \json_decode($request->getContent(), true);
+        if (empty($content = $data['content'])) {
+            throw new AccessDeniedHttpException('No data sent');
+        }
+
+        $channel = $channelRepository->findOneBy([
+            'id' => $data['channel']
+        ]);
+        if (!$channel) {
+            throw new AccessDeniedHttpException('Message have to be sent on a specific channel');
+        }
+
+        try {
+            $content = $this->ollamaClient->getResponse($content);
+        } catch (\Exception $e) {
+            return new Response("Erreur: " . $e->getMessage());
+        }
+
+        $message = new Message();
+        $message->setContent($content["response"]);
+        $message->setChannel($channel);
+        $message->setAuthor($userRepository->find(5));
         $date = new DateTime();
         $message->setCreatedAt($date);
         $message->setUpdatedAt($date);
