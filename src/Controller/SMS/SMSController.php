d97a2709631afb9ae7dd61c8ca62ae42054e437f
Romain LETHUMIER <romain.lethumier@gmail.com>
2024-06-26 07:39:09 +0200

    Update Twilio credentials and SMS content, and refactor SMSController

--- src/Controller/SMS/SMSController.php
+++ src/Controller/SMS/SMSController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller\SMS;
 
+use App\Service\TwilioService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SMSController extends AbstractController
 {
+    private TwilioService $twilioService;
+
+    public function __construct(TwilioService $twilioService)
+    {
+        $this->twilioService = $twilioService;
+    }
+
     #[Route('/envoyer-sms', name: 'app_sms')]
     public function index(): Response
     {
-        return $this->render('sms/index.html.twig', [
-            'controller_name' => 'SMSController',
-        ]);
+        $to = '+330604444761';
+        $body = "Bonjour, ici l'équipe du hackathon ! Vous avez été opéré dernièrement, comment vous sentez-vous ? Dites nous tout ! ";
+
+        try {
+            $sid = $this->twilioService->sendSms($to, $body);
+            return new Response("Message sent with SID: $sid");
+        } catch (\Exception $e) {
+            return new Response("Error: " . $e->getMessage());
+        }
     }
 }
