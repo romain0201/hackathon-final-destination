fe0cb75ea7904525e858cd7c255921d7f6b9338e
Ossama9 <oussama.dahbi98@gmail.com>
2024-06-28 17:03:43 +0200

    order ok

--- src/Entity/Channel.php
+++ src/Entity/Channel.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ChannelRepository;
+use App\Traits\TimestampableTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
